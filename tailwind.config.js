2d08620d998de65bac7fe44ff051d24630f60708
Paolo Trulli <paolotrulli@gmail.com>
2021-10-23 10:35:31 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            spacing: {
+                70: '17.5rem',
+                175: '43.75rem',
+            },
+            maxWidth: {
+                custom: '62.5rem'
+            },
+            fontFamily: {
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
